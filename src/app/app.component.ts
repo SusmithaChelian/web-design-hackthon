04bdfea5dfa6abbe3abf5cfc0ef022d5131bdaef
SusmithaChelian <susmitha.chelian@impelox.com>
2024-08-28 18:08:45 +0530

    design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sample';
+  shoes = [{header:"Nike Shoe",text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",price:"$234"},{header:"Nike Shoe",text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",price:"$234"},{header:"Nike Shoe",text:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",price:"$234"}]
+
+  ngOnInit(): void {
+  }
 }
